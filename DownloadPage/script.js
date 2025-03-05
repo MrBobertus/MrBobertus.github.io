cab1acfe6a9fabdc37e66048fc6a8c3ec5411981
MrBobertus <mr.bobertus.dev@gmail.com>
2025-03-05 09:30:56 +0100

    Update script.js

--- DownloadPage/script.js
+++ DownloadPage/script.js
@@ -9,7 +9,7 @@ async function clicked(button) {
   button.classList.toggle("rotate");
   
   var downloadURL = `https://t4.ftcdn.net/jpg/02/66/72/41/360_F_266724172_Iy8gdKgMa7XmrhYYxLCxyhx6J7070Pr8.jpg`;
-  await wait(3000);
+  await wait(2500);
   window.location.href = downloadURL;
 }
   
