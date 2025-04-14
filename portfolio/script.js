5b7ce770ebe4b2082b0c685ffaa1cb750928d007
MrBobertus <mr.bobertus.dev@gmail.com>
2025-04-14 09:27:30 +0200

    Update script.js

--- portfolio/script.js
+++ portfolio/script.js
@@ -1,5 +1,5 @@
 function redirectProject(type) {
-    location.href = `https://github.com/MrBobertus/${type}`;
+    location.href = `https://github.com/MrBobertus/tree/main/projects/${type}`;
 }
 
 function redirectToWeb(url) {
